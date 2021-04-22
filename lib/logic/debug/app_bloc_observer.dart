5a23b35651797c66f6d8de6255928981b584e6e0
khairul anas <mafiakanan@gmail.com>
2021-04-22 14:52:16 +0700

    upgrade

--- lib/logic/debug/app_bloc_observer.dart
+++ lib/logic/debug/app_bloc_observer.dart
@@ -2,25 +2,27 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
-  void onChange(Cubit cubit, Change change) {
+  void onChange(BlocBase bloc, Change change) {
     print(change);
-    super.onChange(cubit, change);
+    super.onChange(bloc, change);
   }
 
   @override
-  void onClose(Cubit cubit) {
-    super.onClose(cubit);
+  void onClose(BlocBase bloc) {
+    print('$bloc has closed');
+    super.onClose(bloc);
   }
 
   @override
-  void onCreate(Cubit cubit) {
-    print(cubit);
-    super.onCreate(cubit);
+  void onCreate(BlocBase bloc) {
+    print('$bloc has created');
+    super.onCreate(bloc);
   }
 
   @override
-  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
-    super.onError(cubit, error, stackTrace);
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('bloc:$bloc,\nerror:$error,\nstacktrace:$stackTrace');
+    super.onError(bloc, error, stackTrace);
   }
 
   @override
